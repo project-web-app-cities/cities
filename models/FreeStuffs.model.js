2de2121ba304dd330746619284549a88b38e81b5
Jorge Sanchez <jorgesinsilla@gmail.com>
2023-01-05 17:40:34 +0000

    add navbar in layout and    freestuff.model
    Co-authored-by: Thomas Augot <thomasaugot@users.noreply.github.com>

---
+++ models/FreeStuffs.model.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require("mongoose");
+
+const freeStuffsSchema = new Schema({
+  category: {
+    enum: [Museum, Restaurant, Bar, Club, Sport, Events, Transportation, Other],
+    required: [true, "Please select a category"],
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  location: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: String,
+});
+
+
+module.exports = model('FreeStuff', freeStuffsSchema);
