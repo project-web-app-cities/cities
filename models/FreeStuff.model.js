ea20c8bed162f3e460eaa5c948d647a1f9b9da6a
Jorge Sanchez <jorgesinsilla@gmail.com>
2023-01-06 10:56:39 +0000

    change freestuff model

---
+++ models/FreeStuff.model.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require("mongoose");
+
+const freeStuffSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  category: {
+    type: String,
+    enum: ['Museum', 'Restaurant', 'Bar', 'Club', 'Sport', 'Events', 'Transportation', 'Other'],
+    required: [true, "Please select a category"]
+  },
+  location: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+  },
+  image: String,
+});
+
+
+module.exports = model('FreeStuff', freeStuffSchema);
